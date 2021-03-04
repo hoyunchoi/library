13a46b93d2a1c511ddb992e07772de855e103ae9
hoyunchoi <hoyun1009@naver.com>
2021-03-05 00:31:30 +0900

    more efficient binning

--- dataProcess.hpp
+++ dataProcess.hpp
@@ -14,11 +14,15 @@
 
 namespace dataProcess {
 //! Setup variables for binning
-//* Return tuple (minBin, value, binSize)
+//* Return tuple (maxBin, value, binSize)
 const std::tuple<std::vector<int>, std::vector<double>, std::vector<unsigned>> setLogBin_(const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent);
 
-//* Return tuple(minBin, value)
+//* Return tuple(maxBin, value)
 const std::tuple<std::vector<double>, std::vector<double>> setLinBin_(const double& t_minVal, const double& t_maxVal, const double& t_delta);
+
+//! average binning
+template <typename T>
+std::map<double, double> avgBin(const std::map<T, double>& t_raw, const std::vector<double>& t_maxBin, const std::vector<double>& t_value);
 
 //! Log Binning
 //* Log Binning distribution
@@ -44,40 +48,62 @@ std::map<double, double> avgLinBin(const std::map<T, double>& t_raw, const doubl
 
 const std::tuple<std::vector<int>, std::vector<double>, std::vector<unsigned>> dataProcess::setLogBin_(const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
     const std::vector<double> interval = linearAlgebra::elementPow(10.0, linearAlgebra::arange(t_minExponent, t_maxExponent, t_deltaExponent));
-    std::vector<int> minBin;
-    minBin.reserve(interval.size() - 1);
+    std::vector<int> maxBin;
     std::vector<double> value;
-    value.reserve(interval.size() - 1);
     std::vector<unsigned> binSize;
+    maxBin.reserve(interval.size()-1);
+    value.reserve(interval.size() - 1);
     binSize.reserve(interval.size() - 1);
     for (unsigned i = 0; i < interval.size() - 1; ++i) {
         const unsigned size = std::ceil(interval[i + 1]) - std::ceil(interval[i]);
         if (size) {
-            minBin.emplace_back(std::ceil(interval[i]));
+            maxBin.emplace_back(std::ceil(interval[i+1]));
             value.emplace_back(std::sqrt((double)std::ceil(interval[i]) * (double)std::ceil(interval[i + 1])));
             binSize.emplace_back(size);
         }
     }
-    return std::make_tuple(minBin, value, binSize);
+    return std::make_tuple(maxBin, value, binSize);
 }  //* End of function dataProcess::setLogBin_
 
 const std::tuple<std::vector<double>, std::vector<double>> dataProcess::setLinBin_(const double& t_minVal, const double& t_maxVal, const double& t_delta) {
-    const std::vector<double> minBin = linearAlgebra::arange(t_minVal, t_maxVal, t_delta);
+    std::vector<double> maxBin = linearAlgebra::arange(t_minVal, t_maxVal, t_delta);
     std::vector<double> value;
-    value.reserve(minBin.size() - 1);
-    for (unsigned i = 0; i < minBin.size(); ++i) {
-        value.emplace_back((minBin[i] + minBin[i + 1]) / 2.0);
+    value.reserve(maxBin.size() - 1);
+    for (unsigned i = 0; i < maxBin.size()-1; ++i) {
+        value.emplace_back((maxBin[i] + maxBin[i + 1]) / 2.0);
     }
-    return std::make_tuple(minBin, value);
+    maxBin.erase(maxBin.begin());
+    return std::make_tuple(maxBin, value);
 }  //* End of function dataProcess::setLinBin_
+
+
+template <typename T>
+std::map<double, double> dataProcess::avgBin(const std::map<T, double>& t_raw, const std::vector<double>& t_maxBin, const std::vector<double>& t_value){
+    std::map<double, double> binned;
+    std::map<double, unsigned> sampled;
+    for (const auto& raw : t_raw) {
+        for (unsigned j = 0; j < t_value.size(); ++j) {
+            if (raw.first < t_maxBin[j]) {
+                binned[t_value[j]] += raw.second;
+                ++sampled[t_value[j]];
+                break;
+            }
+        }
+    }
+    for (const auto& e : sampled) {
+        binned.at(e.first) /= e.second;
+    }
+    return binned;
+}
+
 
 std::map<double, double> dataProcess::distLogBin(const std::vector<double>& t_raw, const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
     using namespace linearAlgebra;
     std::map<double, double> binned;
-    const auto [minBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
+    const auto [maxBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
     for (unsigned i = 0; i < t_raw.size(); ++i) {
         for (unsigned j = 0; j < value.size(); ++j) {
-            if ((int)i < minBin[j + 1]) {
+            if ((int)i < maxBin[j]) {
                 binned[value[j]] += t_raw[i] / (double)binSize[j];
                 break;
             }
@@ -94,10 +120,10 @@ template <typename T>
 std::map<double, double> dataProcess::distLogBin(const std::map<T, double>& t_raw, const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
     using namespace linearAlgebra;
     std::map<double, double> binned;
-    const auto [minBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
+    const auto [maxBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
     for (const auto& raw : t_raw) {
         for (unsigned j = 0; j < value.size(); ++j) {
-            if (raw.first < minBin[j + 1]) {
+            if (raw.first < maxBin[j]) {
                 binned[value[j]] += raw.second / (double)binSize[j];
                 break;
             }
@@ -108,13 +134,13 @@ std::map<double, double> dataProcess::distLogBin(const std::map<T, double>& t_ra
 }
 
 std::map<double, double> dataProcess::avgLogBin(const std::vector<double>& t_raw, const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
+    const auto [maxBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
+    (void)binSize;
     std::map<double, double> binned;
     std::map<double, unsigned> sampled;
-    const auto [minBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
-    (void)binSize;
     for (unsigned i = 0; i < t_raw.size(); ++i) {
         for (unsigned j = 0; j < value.size(); ++j) {
-            if ((int)i < minBin[j + 1]) {
+            if ((int)i < maxBin[j]) {
                 binned[value[j]] += t_raw[j];
                 ++sampled[value[j]];
                 break;
@@ -129,34 +155,19 @@ std::map<double, double> dataProcess::avgLogBin(const std::vector<double>& t_raw
 
 template <typename T>
 std::map<double, double> dataProcess::avgLogBin(const std::map<T, double>& t_raw, const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
-    std::map<double, double> binned;
-    std::map<double, unsigned> sampled;
-    const auto [minBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
-    (void)binSize;
-    for (const auto& raw : t_raw) {
-        for (unsigned j = 0; j < value.size(); ++j) {
-            if (raw.first < minBin[j + 1]) {
-                binned[value[j]] += raw.second;
-                ++sampled[value[j]];
-                break;
-            }
-        }
-    }
-    for (const auto& e : sampled) {
-        binned.at(e.first) /= e.second;
-    }
-    return binned;
+    const auto [maxBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
+    return avgBin(t_raw, maxBin, value);
 }  //* End of function dataProcess::logBin(map)
 
 std::map<std::pair<double, double>, double> dataProcess::distLogLogBin(const std::map<std::pair<int, int>, double>& t_raw, const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
     using namespace linearAlgebra;
     std::map<std::pair<double, double>, double> binned;
-    const auto [minBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
+    const auto [maxBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
     for (const auto& raw : t_raw) {
         for (unsigned j = 0; j < value.size(); ++j) {
-            if (raw.first.first < minBin[j + 1]) {
+            if (raw.first.first < maxBin[j]) {
                 for (unsigned k = 0; k < value.size(); ++k) {
-                    if (raw.first.second < minBin[k + 1]) {
+                    if (raw.first.second < maxBin[k]) {
                         binned[std::make_pair(value[j], value[k])] += raw.second / (binSize[j] * binSize[k]);
                         break;
                     }
@@ -171,13 +182,13 @@ std::map<std::pair<double, double>, double> dataProcess::distLogLogBin(const std
 std::map<std::pair<double, double>, double> dataProcess::avgLogLogBin(const std::map<std::pair<int, int>, double>& t_raw, const double& t_minExponent, const double& t_maxExponent, const double& t_deltaExponent) {
     std::map<std::pair<double, double>, double> binned;
     std::map<std::pair<double, double>, unsigned> sampled;
-    const auto [minBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
+    const auto [maxBin, value, binSize] = setLogBin_(t_minExponent, t_maxExponent, t_deltaExponent);
     (void)binSize;
     for (const auto& raw : t_raw) {
         for (unsigned j = 0; j < value.size(); ++j) {
-            if (raw.first.first < minBin[j + 1]) {
+            if (raw.first.first < maxBin[j]) {
                 for (unsigned k = 0; k < value.size(); ++k) {
-                    if (raw.first.second < minBin[k + 1]) {
+                    if (raw.first.second < maxBin[k]) {
                         binned[std::make_pair(value[j], value[k])] += raw.second;
                         ++sampled[std::make_pair(value[j], value[k])];
                         break;
@@ -196,10 +207,10 @@ template <typename T>
 std::map<double, double> dataProcess::distLinBin(const std::map<T, double>& t_raw, const double& t_minVal, const double& t_maxVal, const double& t_delta) {
     using namespace linearAlgebra;
     std::map<double, double> binned;
-    const auto [minBin, value] = setLinBin_(t_minVal, t_maxVal, t_delta);
+    const auto [maxBin, value] = setLinBin_(t_minVal, t_maxVal, t_delta);
     for (const auto& raw : t_raw) {
         for (unsigned j = 0; j < value.size(); ++j) {
-            if (raw.first < minBin[j + 1]) {
+            if (raw.first < maxBin[j]) {
                 binned[value[j]] += raw.second;
                 break;
             }
@@ -211,20 +222,6 @@ std::map<double, double> dataProcess::distLinBin(const std::map<T, double>& t_ra
 
 template <typename T>
 std::map<double, double> dataProcess::avgLinBin(const std::map<T, double>& t_raw, const double& t_minVal, const double& t_maxVal, const double& t_delta) {
-    std::map<double, double> binned;
-    std::map<double, unsigned> sampled;
-    const auto [minBin, value] = setLinBin_(t_minVal, t_maxVal, t_delta);
-    for (const auto& raw : t_raw) {
-        for (unsigned j = 0; j < value.size(); ++j) {
-            if (raw.first < minBin[j + 1]) {
-                binned[value[j]] += raw.second;
-                ++sampled[value[j]];
-                break;
-            }
-        }
-    }
-    for (const auto& e : sampled) {
-        binned.at(e.first) /= e.second;
-    }
-    return binned;
+    const auto [maxBin, value] = setLinBin_(t_minVal, t_maxVal, t_delta);
+    return avgBin(t_raw, maxBin, value);
 }  //* End of function dataProcess::avgLinBin
--- dataProcess.py
+++ dataProcess.py
@@ -3,81 +3,76 @@ import numpy as np
 #* Log binning
 
 
-def intLogBin(raw_x, raw_y):
-    assert len(raw_x) == len(
-        raw_y), "At int log binning, need same length of array"
+def distLogBin(raw_x, raw_y, min_exponent=0.0, max_exponent=10.0, delta_exponent=0.1):
+    assert len(raw_x) == len(raw_y), "At dist log binning, need same length of array"
+    assert type(raw_x) is np.ndarray, "At avg lin binning, input need to be numpy array"
+    assert type(raw_y) is np.ndarray, "At avg lin binning, input need to be numpy array"
 
-    #* Setup values for double log binning
-    min = np.logspace(0, 10, num=101, base=10)
-    value = np.sqrt(min[1:] * min[:-1])
-
-    #* Bin the data
-    binned = {}
-    sampled = {}
-    for i in range(len(raw_x)):
-        for j in range(len(value)):
-            if raw_x[i] < min[j]:
-                if value[j] in binned:
-                    binned[value[j]] += raw_y[i]
-                    sampled[value[j]] += 1
-                else:
-                    binned[value[j]] = raw_y[i]
-                    sampled[value[j]] = 1
-                break
-    for value in binned:
-        binned[value] /= sampled[value]
-    return np.array(list(binned.keys())), np.array(list(binned.values()))
+    bins = np.ceil(np.logspace(min_exponent, max_exponent, base=10.0, num=int((max_exponent - min_exponent) / delta_exponent) + 1))
+    values = np.sqrt(bins[1:] * bins[:-1])
+    binSize = bins[1:] - bins[:-1]
+    digitized = np.digitize(raw_x, bins=bins, right=False)
+    binned = np.zeros_like(values)
+    for i in range(1, len(bins)):
+        temp = np.sum(raw_y[digitized==i])
+        if temp.size != 0:
+            binned[i - 1] = np.sum(temp) / binSize[i - 1]
+    values = values[binned != 0]
+    binned = binned[binned != 0]
+    return values, binned / np.sum(binned)
 
 
-def doubleLogBin(raw_x, raw_y):
-    assert len(raw_x) == len(
-        raw_y), "At double log binning, need same length of array"
+def avgLogBin(raw_x, raw_y, min_exponent=0.0, max_exponent=10.0, delta_exponent=0.1):
+    assert len(raw_x) == len(raw_y), "At avg log binning, need same length of array"
+    assert type(raw_x) is np.ndarray, "At avg lin binning, input need to be numpy array"
+    assert type(raw_y) is np.ndarray, "At avg lin binning, input need to be numpy array"
 
-    #* Setup values for double log binning
-    min = np.logspace(-10, 0, num=101, base=10)
-    value = np.sqrt(min[1:] * min[:-1])
-
-    #* Bin the data
-    binned = {}
-    sampled = {}
-    for i in range(len(raw_x)):
-        for j in range(len(value)):
-            if raw_x[i] < min[j]:
-                if value[j] in binned:
-                    binned[value[j]] += raw_y[i]
-                    sampled[value[j]] += 1
-                else:
-                    binned[value[j]] = raw_y[i]
-                    sampled[value[j]] = 1
-                break
-    for value in binned:
-        binned[value] /= sampled[value]
-    return np.array(list(binned.keys())), np.array(list(binned.values()))
+    bins = np.ceil(np.logspace(min_exponent, max_exponent, base=10.0, num=int((max_exponent - min_exponent) / delta_exponent) + 1))
+    values = np.sqrt(bins[1:] * bins[:-1])
+    digitized = np.digitize(raw_x, bins=bins, right=False)
+    binned = np.zeros_like(values)
+    for i in range(1, len(bins)):
+        temp = raw_y[digitized == i]
+        if temp.size != 0:
+            binned[i - 1] = np.mean(raw_y[digitized == i])
+    values = values[binned != 0]
+    binned = binned[binned != 0]
+    return values, binned
 
 #* Linear binning
+def distLinBin(raw_x, raw_y, min_val=0.0, max_val=1.0, delta=5e-4):
+    assert len(raw_x) == len(raw_y), "At avg lin binning, need same length of array"
+    assert type(raw_x) is np.ndarray, "At avg lin binning, input need to be numpy array"
+    assert type(raw_y) is np.ndarray, "At avg lin binning, input need to be numpy array"
+
+    bins = np.arange(min_val, max_val + delta, delta)
+    values = (bins[1:] + bins[:-1]) / 2.0
+    digitized = np.digitize(raw_x, bins=bins, right=False)
+    binned = np.zeros_like(values)
+    for i in range(1, len(bins)):
+        temp = raw_y[digitized == i]
+        if temp.size != 0:
+            binned[i - 1] = np.sum(temp)
+    values = values[binned != 0]
+    binned = binned[binned != 0]
+    return values, binned / np.sum(binned)
+
+def avgLinBin(raw_x, raw_y, min_val=0.0, max_val=1.0, delta=5e-4):
+    assert len(raw_x) == len(raw_y), "At avg lin binning, need same length of array"
+    assert type(raw_x) is np.ndarray, "At avg lin binning, input need to be numpy array"
+    assert type(raw_y) is np.ndarray, "At avg lin binning, input need to be numpy array"
+
+    bins = np.arange(min_val, max_val + delta, delta)
+    values = (bins[1:] + bins[:-1]) / 2.0
+    digitized = np.digitize(raw_x, bins=bins, right=False)
+    binned = np.zeros_like(values)
+    for i in range(1, len(bins)):
+        temp = raw_y[digitized == i]
+        if temp.size != 0:
+            binned[i - 1] = np.mean(temp)
+    return values[binned != 0], binned[binned != 0]
 
 
-def doubleLinBin(raw_x, raw_y):
-    assert len(raw_x) == len(
-        raw_y), "At double lin binning, need same length of array"
-
-    #* Setup values for double linear binning
-    min = np.linspace(0.0, 1.0, 1001)
-    value = (min[1:] + min[:-1]) / 2.0
-
-    #* Bin the data
-    binned = {}
-    sampled = {}
-    for i in range(len(raw_x)):
-        for j in range(len(value)):
-            if raw_x[i] < min[j]:
-                if value[j] in binned:
-                    binned[value[j]] += raw_y[i]
-                    sampled[value[j]] += 1
-                else:
-                    binned[value[j]] = raw_y[i]
-                    sampled[value[j]] = 1
-                break
-    for value in binned:
-        binned[value] /= sampled[value]
-    return np.array(list(binned.keys())), np.array(list(binned.values()))
+if __name__ == "__main__":
+    a= np.array([])
+    print(a.is_empty())
